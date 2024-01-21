a344db77e7282536c8633e33d6dbcf2c891a2a62
X-okSync <156274247+xoksync@users.noreply.github.com>
2024-01-21 13:45:56 -0300

    (add): support for bmp280 module

--- Flipper-Ino-A03.ino
+++ Flipper-Ino-A03.ino
@@ -1,4 +1,6 @@
 #include <U8glib.h>
+#include <Adafruit_Sensor.h> //INCLUSÃO DE BIBLIOTECA
+#include <Adafruit_BMP280.h>
 
 #include "Option.hpp"
 #include "Display.hpp"
@@ -15,15 +17,23 @@ uint32_t system_running_tick;
 Button down(4), up(6), enter(5);
 MenuSystem menu_system_home, menu_system_tools;
 Page *current_page;
+Adafruit_BMP280 bmp;
 
 namespace Pages
-{ Page home, tools, about, bitcoin; };
+{ Page home, tools, about, bitcoin, temperature; };
 
 void boot_system() noexcept
 {
 	display.firstPage(); do {
 		display.draw(Texture(SPLASH_SCREEN_BOOT, 128, 64));
 	} while( display.nextPage() );
+
+	while(!bmp.begin(0x76)) { delay(100); }
+
+	down.setup();
+	up.setup();
+	enter.setup();
+
 	delay(2000);
 }
 void switchPage(Page &page) noexcept
@@ -36,10 +46,6 @@ void setup(void)
 	display.create();
 	switchPage(Pages::home);
 	boot_system();
-
-	down.setup();
-	up.setup();
-	enter.setup();
 
 	menu_system_home.setDisplay(&display);
 	menu_system_home.addOption(Menu::Option("Tools", ICON_TOOLS, []()->void{switchPage(Pages::tools);}));
@@ -54,13 +60,24 @@ void setup(void)
 	menu_system_tools.addOption(Menu::Option("Bad USB", ICON_SCRIPT));
 	menu_system_tools.addOption(Menu::Option("Wi-Fi Spam", ICON_SCRIPT));
 	menu_system_tools.addOption(Menu::Option("DAUTHER", ICON_SCRIPT));
-	menu_system_tools.addOption(Menu::Option("Bitcoin", ICON_SCRIPT, []()->void{switchPage(Pages::bitcoin);}));
+	menu_system_tools.addOption(Menu::Option("Temperature", ICON_SCRIPT, []()->void{switchPage(Pages::temperature);}));
+	menu_system_tools.addOption(Menu::Option("Bitcoin Wallet", ICON_SCRIPT, []()->void{switchPage(Pages::bitcoin);}));
 	menu_system_tools.addOption(Menu::Option("Back", ICON_BACK, []()->void{switchPage(Pages::home);}));
 
 	Pages::bitcoin.create([]()->void{
 		display.firstPage(); do {
 			display.draw(Texture(PLEASE_DONT_SEND_BITCOIN, 128, 64));
 		} while( display.nextPage() );
+		if(enter.isPressed()){ delay(250); switchPage(Pages::tools); }
+	});
+
+	Pages::temperature.create([]()->void{
+		display.firstPage(); do {
+			display.draw(Text(("Temp: " + String(bmp.readTemperature()) + " *C").c_str(), 12, 12));
+			display.draw(Text(("Pres: " + String(bmp.readPressure()) + " ").c_str(), 12, 12 * 2));
+			display.draw(Text(("Alti: " + String(bmp.readAltitude(1013.25)) + "m").c_str(), 12, 12 * 3));
+		} while( display.nextPage() );
+		if(enter.isPressed()){ delay(250); switchPage(Pages::tools); }
 	});
 
 	Pages::tools.create([]()->void {
@@ -72,20 +89,17 @@ void setup(void)
 
 	Pages::about.create([]()->void {
 		static int t = 5;
-
 		display.firstPage(); do {
 			display.draw(Text("Created by X-okSync", 0, 12));
 			display.draw(Text("Pay me a coffee... pls", 0, 12 * 2));
 			display.draw(Text(("Exting " + String(t) + "...").c_str(), 0, 12 * 5));
 		} while( display.nextPage() );
-
 		delay(1000);
 		if(t-- == 1)
 		{
 			t = 5;
 			switchPage(Pages::home);
 		}
-
 	});
 	
 	Pages::home.create([]()->void {
@@ -99,6 +113,6 @@ void setup(void)
 void loop(void)
 {
   current_page->loop();
-	delay(1000 / 24);
+	delay(1000 / 30);
 }
 
